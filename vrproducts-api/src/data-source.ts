69214b0fa127fb8d29dbe87fd283125ad57110d8
Igor <silveira.igor@outlook.com>
2023-11-09 14:28:58 -0300

    feat: add three entities and migration for creation of db

--- vrproducts-api/src/data-source.ts
+++ vrproducts-api/src/data-source.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import "dotenv/config";
 import path from "path";
 import { DataSource, DataSourceOptions } from "typeorm";
---
+++ vrproducts-api/src/entities/product.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import ProductStore from "./productStore.entity";
+
+@Entity("products")
+class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 60 })
+  description: string;
+
+  @Column({ type: "numeric", precision: 13, scale: 3, nullable: true })
+  price: number;
+
+  @Column({ type: "bytea", nullable: true })
+  image: string;
+
+  @OneToMany(() => ProductStore, (productStore) => productStore.product)
+  productsStore: ProductStore[];
+}
+
+export default Product;
