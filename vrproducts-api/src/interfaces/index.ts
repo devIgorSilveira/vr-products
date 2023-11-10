04d345e16763a0c463c4fd5af6a33552c44141c5
Igor <silveira.igor@outlook.com>
2023-11-10 14:24:05 -0300

    feat: add interfaces and schemas for store

--- vrproducts-api/src/interfaces/index.ts
+++ vrproducts-api/src/interfaces/index.ts
@@ -4,10 +4,13 @@ import {
   IQueryProductsInterface,
   IProductUpdateInterface,
 } from "./product";
+import { IStoreRequestInterface, IStoreInterface } from "./store";
 
 export {
   IProductInterface,
   IProductRequestInterface,
   IQueryProductsInterface,
   IProductUpdateInterface,
+  IStoreRequestInterface,
+  IStoreInterface,
 };
