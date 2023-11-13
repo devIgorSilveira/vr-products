24c1c41ba9ff96031d3f05becf760b24c1084c83
Igor <silveira.igor@outlook.com>
2023-11-13 16:31:22 -0300

    fix: fix problem when delete a product with sale in store

--- vrproducts-api/src/services/products/deleteProduct.service.ts
+++ vrproducts-api/src/services/products/deleteProduct.service.ts
@@ -1,9 +1,27 @@
 import { Repository } from "typeorm";
-import { Product } from "../../entities";
+import { Product, ProductStore } from "../../entities";
 import appDataSource from "../../data-source";
 
 const deleteProductService = async (id: number): Promise<Object> => {
   const productRepo: Repository<Product> = appDataSource.getRepository(Product);
+
+  const product = await productRepo.findOne({
+    where: {
+      id: id,
+    },
+    relations: {
+      productsStore: true,
+    },
+  });
+
+  if (product!.productsStore.length > 0) {
+    const productStoreRepo: Repository<ProductStore> =
+      appDataSource.getRepository(ProductStore);
+
+    product?.productsStore.forEach(async (registry) => {
+      await productStoreRepo.delete({ id: registry.id });
+    });
+  }
 
   await productRepo.delete({ id: id });
 
